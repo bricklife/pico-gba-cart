6d70510cbe939cb162187be13d2ac4c8fa66f588
Charlie Birks <charlie@daft.games>
2024-01-08 12:48:59 +0000

    Also remove colour bars

--- gba/main.c
+++ gba/main.c
@@ -25,23 +25,6 @@ int main() {
 
     REG_DISPCNT = VIDEO_MODE_BITMAP | VIDEO_BG2_ENABLE;
 
-    for(int y = 0; y < 16; y++) {
-        for(int x = 0; x < 240; x++) {
-            int v = x / 8;
-            MODE3_FRAME[y][x] = v;
-            MODE3_FRAME[y + 16][x] = v << 5;
-            MODE3_FRAME[y + 32][x] = v << 10;
-
-            MODE3_FRAME[y + 48][x] = v | v << 5;
-            MODE3_FRAME[y + 64][x] = v | v << 10;
-            MODE3_FRAME[y + 80][x] = v << 5 | v << 10;
-
-            MODE3_FRAME[y + 96][x] = v | v << 5 | v << 10;
-
-            MODE3_FRAME[y + 112][x] = 0x7FFF;
-        }
-    }
-
     // enable irq/vblank
     irqInitDefault();
     irqEnable(IRQ_VBLANK);
